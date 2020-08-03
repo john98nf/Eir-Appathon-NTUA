4212a127165a04e07a61ebdb090b2c4a966ae977
Giannis Fakinos <giannhs.afasias@gmail.com>
2020-08-03 23:32:37 +0300

    WIP: Basic back-end server (No connection with db wet)

---
+++ back-end/src/test/java/gr/ntua/ece/internetappli/eir/EirApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.ntua.ece.internetappli.eir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
